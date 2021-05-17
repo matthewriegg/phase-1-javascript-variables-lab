8124b5df0b2852065163866d8ee47f14e61773b8
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-05-17 10:27:54 -0700

    deliverables

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+
+// //- `companyName` &mdash; Inside the `test/indexTest.js` file, look at the
+// `describe` function call for the `companyName` variable. The two `it` function
+// calls inside this `describe` tell us the features of `companyName` we need to
+// create. To review, in the first `it` function call, it says that `it`
+// (companyName) `is set as Scuber`. In the next line, you can see that the test
+// checks to make sure this occurs by seeing if `companyName` equals `Scuber`. So
+// this means that you need to go to your `index.js` file and declare a variable
+// named `companyName` and set it equal to `Scuber`.
+// - In the second `it` function call for `companyName`, it says it `is defined as
+// a const`. The next line of code tests this. So you need to make sure that you
+// are using the correct type of variable declaration such that attempting to
+// reassign the variable throws an error.
+// - `mostProfitableNeighborhood` and `companyCeo` &mdash; Here we are getting more
+// practice with declaring variables. Read the tests to see how you need to code
+// these two variables to get the remaining tests passing.
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
+
+
+
+
